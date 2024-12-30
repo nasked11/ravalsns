e25bce31d4ad88af851e40eb949a3aebe1d5f302
Eduardo Nc <nasked@rosarium.local>
2024-12-30 15:02:29 -0600

    Update data value

--- src/utils/dummyData.js
+++ src/utils/dummyData.js
@@ -499,7 +499,7 @@ export const data = [
         "/assets/product-zx9-speaker/desktop/image-category-page-preview.jpg",
     },
     new: true,
-    price: 4500,
+    price: 9900,
     description:
       "Upgrade your sound system with the all new ZX9 active speaker. It’s a bookshelf speaker system that offers truly wireless connectivity -- creating new possibilities for more pleasing and practical audio setups.",
     features:
